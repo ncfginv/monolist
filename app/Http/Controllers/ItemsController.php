a5345a01071b0154d122877063195c2b4646640c
takayuki <taka4852@i.softbank.jp>
2017-08-23 14:26:39 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -8,7 +8,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Item;
 
-class WelcomeController extends Controller
+class ItemsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -17,10 +17,7 @@ class WelcomeController extends Controller
      */
     public function index()
     {
-       $items = Item::orderBy('updated_at', 'desc')->paginate(20);
-        return view('welcome', [
-            'items' => $items,
-        ]);
+        //
     }
 
     /**
@@ -30,7 +27,33 @@ class WelcomeController extends Controller
      */
     public function create()
     {
-        //
+        $keyword = request()->keyword;
+        $items = [];
+        if ($keyword) {
+            $client = new \RakutenRws_Client();
+            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+
+            $rws_response = $client->execute('IchibaItemSearch', [
+                'keyword' => $keyword,
+                'imageFlag' => 1,
+                'hits' => 20,
+            ]);
+
+            // 扱い易いように Item としてインスタンスを作成する（保存はしない）
+            foreach ($rws_response->getData()['Items'] as $rws_item) {
+                $item = new Item();
+                $item->code = $rws_item['Item']['itemCode'];
+                $item->name = $rws_item['Item']['itemName'];
+                $item->url = $rws_item['Item']['itemUrl'];
+                $item->image_url = str_replace('?_ex=128x128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
+                $items[] = $item;
+            }
+        }
+
+        return view('items.create', [
+            'keyword' => $keyword,
+            'items' => $items,
+        ]);
     }
 
     /**
@@ -52,7 +75,13 @@ class WelcomeController extends Controller
      */
     public function show($id)
     {
-        //
+         $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
 
     /**
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use App\Item;
+use App\User;
 
-class WelcomeController extends Controller
+class UsersController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -17,10 +17,7 @@ class WelcomeController extends Controller
      */
     public function index()
     {
-       $items = Item::orderBy('updated_at', 'desc')->paginate(20);
-        return view('welcome', [
-            'items' => $items,
-        ]);
+        //
     }
 
     /**
@@ -52,8 +49,18 @@ class WelcomeController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::find($id);
+        $count_want = $user->want_items()->count();
+        $items = \DB::table('items')->join('item_user', 'items.id', '=', 'item_user.item_id')->select('items.*')->where('item_user.user_id', $user->id)->distinct()->groupBy('items.id')->paginate(20);
+
+        return view('users.show', [
+            'user' => $user,
+            'items' => $items,
+            'count_want' => $count_want,
+            // 'count_have' => $count_have,
+        ]);
     }
+    
 
     /**
      * Show the form for editing the specified resource.
